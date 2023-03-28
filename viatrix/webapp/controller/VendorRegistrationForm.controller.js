23259abf5237ba092f87319cccac414dbd7dc589
Nihar Ranjan Rout <rout.nihar04@gmail.com>
2023-03-28 10:58:15 +0000

    added add section function

--- viatrix/webapp/controller/VendorRegistrationForm.controller.js
+++ viatrix/webapp/controller/VendorRegistrationForm.controller.js
@@ -1,12 +1,12 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller", 
+    "sap/ui/core/mvc/Controller",
     "viatrix/utils/formatter",
     "sap/ui/core/routing/History"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller,formatter,History,Hello) {
+    function (Controller, formatter, History, Hello) {
         "use strict";
 
         return Controller.extend("viatrix.controller.VendorRegistrationForm", {
@@ -23,21 +23,21 @@ sap.ui.define([
                 this.sResolvedURI = this.sResolvedURI === "./" ? "" : this.sResolvedURI;
 
 
-                var history= History.getInstance();
-                console.log("history- "+history.aHistory);
+                var history = History.getInstance();
+                console.log("history- " + history.aHistory);
 
                 var topNavigationBreadCrumb = this.getView().byId("topNavigation");
-                for(let i=0;i<history.aHistory.length-1;i++){
-                    let tLink= new sap.m.Link({
+                for (let i = 0; i < history.aHistory.length - 1; i++) {
+                    let tLink = new sap.m.Link({
                         press: "onPress",
                         text: "Search Existing Vendor"
                     }).addStyleClass("TopNavigationPastLink");
                     topNavigationBreadCrumb.addLink(tLink);
                 }
-                let cLink= new sap.m.Link({
-                    text: history.aHistory[history.aHistory.length-1]
+                let cLink = new sap.m.Link({
+                    text: history.aHistory[history.aHistory.length - 1]
                 });
-                topNavigationBreadCrumb.setCurrentLocationText(history.aHistory[history.aHistory.length-1]);
+                topNavigationBreadCrumb.setCurrentLocationText(history.aHistory[history.aHistory.length - 1]);
 
 
                 var oActivityLog = this.getOwnerComponent().getModel("oActivityLog");
@@ -50,13 +50,13 @@ sap.ui.define([
                 this.oSupplierDetailModel = oSupplierDetailModel;
 
 
-                oSupplierDetailModel.setProperty("/companyCodeInfo",[]);
+                oSupplierDetailModel.setProperty("/companyCodeInfo", []);
 
 
 
-                oActivityLog.setProperty("/test","test");
-                oActivityLog.setProperty("/word1","General");
-                oActivityLog.setProperty("/word2","Information");
+                oActivityLog.setProperty("/test", "test");
+                oActivityLog.setProperty("/word1", "General");
+                oActivityLog.setProperty("/word2", "Information");
 
                 var custDate = new Date();
                 custDate.setDate(custDate.getDate() + 30);
@@ -65,7 +65,7 @@ sap.ui.define([
                 }), "MinDate");
                 // oVendorDetailModel.setProperty("/companyCodeInfo/validUntil", custDate);
                 // oVendorDetailModel.refresh();
-                console.log("valid until"+ oSupplierDetailModel.getProperty("/validUntil"));
+                console.log("valid until" + oSupplierDetailModel.getProperty("/validUntil"));
 
                 this._oWizard = this.byId("vendorRegistrationFormWizard");
                 this._iSelectedStepIndex = 0;
@@ -74,12 +74,12 @@ sap.ui.define([
                 //this.getVendorDetail();
             },
 
-            onTestNavigate: function(){
+            onTestNavigate: function () {
                 this.router.navTo("TaskDetails");
             },
-            handleButtonsVisibility: function(){
+            handleButtonsVisibility: function () {
                 var oButtonmodel = this.oButtonmodel;
-                if(this._iSelectedStepIndex===0){
+                if (this._iSelectedStepIndex === 0) {
                     oButtonmodel.setProperty("/cancelButtonVisible", true);
                     oButtonmodel.setProperty("/backButtonVisible", false);
                     oButtonmodel.setProperty("/saveButtonVisible", true);
@@ -87,14 +87,14 @@ sap.ui.define([
                     oButtonmodel.setProperty("/previewButtonVisible", false);
                     oButtonmodel.setProperty("/submitButtonVisible", false);
 
-                }else if (this._iSelectedStepIndex===5){
+                } else if (this._iSelectedStepIndex === 5) {
                     oButtonmodel.setProperty("/cancelButtonVisible", false);
                     oButtonmodel.setProperty("/backButtonVisible", true);
                     oButtonmodel.setProperty("/saveButtonVisible", true);
                     oButtonmodel.setProperty("/nextButtonVisible", false);
                     oButtonmodel.setProperty("/previewButtonVisible", true);
                     oButtonmodel.setProperty("/submitButtonVisible", true);
-                }else{
+                } else {
                     oButtonmodel.setProperty("/cancelButtonVisible", false);
                     oButtonmodel.setProperty("/backButtonVisible", true);
                     oButtonmodel.setProperty("/saveButtonVisible", true);
@@ -113,7 +113,7 @@ sap.ui.define([
 
                 var oSupplierDetailModel = this.oSupplierDetailModel;
                 var oVendorDetailModel = this.oVendorDetailModel;
-                console.log("valid until"+ oSupplierDetailModel.getProperty("/validUntil"));
+                console.log("valid until" + oSupplierDetailModel.getProperty("/validUntil"));
 
 
                 this._iSelectedStepIndex = this._oWizard.getSteps().indexOf(this._oSelectedStep);
@@ -146,16 +146,16 @@ sap.ui.define([
                 //         this.handleButtonsVisibility();
                 //     }
                 // } else {
-                    var oNextStep = this._oWizard.getSteps()[this._iSelectedStepIndex + 1];
-                    if (this._oSelectedStep && !this._oSelectedStep.bLast) {
-                        this._oWizard.goToStep(oNextStep, true);
-                    } else {
-                        this._oWizard.nextStep();
-                    }
-                    this._iSelectedStepIndex++;
-                    this._oSelectedStep = oNextStep;
+                var oNextStep = this._oWizard.getSteps()[this._iSelectedStepIndex + 1];
+                if (this._oSelectedStep && !this._oSelectedStep.bLast) {
+                    this._oWizard.goToStep(oNextStep, true);
+                } else {
+                    this._oWizard.nextStep();
+                }
+                this._iSelectedStepIndex++;
+                this._oSelectedStep = oNextStep;
 
-                    this.handleButtonsVisibility();
+                this.handleButtonsVisibility();
                 // }
 
 
@@ -175,20 +175,20 @@ sap.ui.define([
 
                 this.handleButtonsVisibility();
             },
-            onClickAddSectionCompanyCodeInformation: function(oEvent){
+            onClickAddSectionCompanyCodeInformation: function (oEvent) {
                 var companyCodeInformationVbox = this.getView().byId("companyCodeInformationVbox");
                 var testPanel = this.getView().byId("panel1");
-                var idArray=["9","8","7"];
-                testPanel.clone("lk",idArray);
+                var idArray = ["9", "8", "7"];
+                testPanel.clone("lk", idArray);
 
-                var vbox1= new sap.m.VBox()
-                   .addItem(new sap.m.Label({
-                    text: "Company Code",
-                    required: true
-                })).addItem(new sap.m.Input({
-                    value: "{oVendorDetailModel>companyCode}"
-                }).addStyleClass("voInputText"));
-                var vbox2= new sap.m.VBox().addItem(new sap.m.Label({
+                var vbox1 = new sap.m.VBox()
+                    .addItem(new sap.m.Label({
+                        text: "Company Code",
+                        required: true
+                    })).addItem(new sap.m.Input({
+                        value: "{oVendorDetailModel>companyCode}"
+                    }).addStyleClass("voInputText"));
+                var vbox2 = new sap.m.VBox().addItem(new sap.m.Label({
                     text: "Reconciliation Account",
                     required: true
                 })).addItem(new sap.m.Input().addStyleClass("voInputText"));
@@ -197,7 +197,7 @@ sap.ui.define([
                     defaultSpan: "XL3 L3 M6 S12",
                     hSpacing: 2
                 }).addContent(vbox1).addContent(vbox2);
-                var mVbox= new sap.m.VBox().addItem(lGrid);
+                var mVbox = new sap.m.VBox().addItem(lGrid);
                 var mPanel = new sap.m.Panel({
                     expandable: true,
                     expanded: true,
@@ -208,20 +208,20 @@ sap.ui.define([
                 companyCodeInformationVbox.addItem(testPanel);
 
             },
-            getVendorDetail: function(){
+            getVendorDetail: function () {
                 var oVendorDetailModel = this.oVendorDetailModel;
 
-                var sUrl = "/vendorDetails/getVendorDetails/VM000004" ;
+                var sUrl = "/vendorDetails/getVendorDetails/VM000004";
                 // var url = that.sResolvedURI + "Viatris_Vendor_Onboarding" + sUrl;
                 jQuery.ajax({
                     url: sUrl,
                     type: "GET",
                     success: function (data) {
-                        
+
                         oVendorDetailModel.setProperty("/", data.data);
-                        console.log("success1 "+oVendorDetailModel.getProperty("/name1"));
-                        var arr= oVendorDetailModel.getProperty("/address/mainAddress/street");
-                        console.log("success 333"+arr);
+                        console.log("success1 " + oVendorDetailModel.getProperty("/name1"));
+                        var arr = oVendorDetailModel.getProperty("/address/mainAddress/street");
+                        console.log("success 333" + arr);
 
                     },
                     error: function (oResp) {
@@ -229,11 +229,11 @@ sap.ui.define([
                     }
                 })
             },
-            postVendorDetails: function(){
+            postVendorDetails: function () {
                 var oVendorDetailModel = this.oVendorDetailModel;
                 var supplierData = oVendorDetailModel.getData();
-                    var oData = jQuery.extend(true, {}, supplierData);
-                var sUrl = "/vendorDetails/createVendorDetails" ;
+                var oData = jQuery.extend(true, {}, supplierData);
+                var sUrl = "/vendorDetails/createVendorDetails";
                 jQuery.ajax({
                     url: sUrl,
                     method: "POST",
@@ -241,9 +241,9 @@ sap.ui.define([
                     contentType: "application/json; charset=UTF-8",
                     success: function (data) {
                         oVendorDetailModel.setProperty("/", data.data);
-                        console.log("success1 "+oVendorDetailModel.getProperty("/name1"));
-                        var arr= oVendorDetailModel.getProperty("/address/mainAddress/street");
-                        console.log("success 333"+arr);
+                        console.log("success1 " + oVendorDetailModel.getProperty("/name1"));
+                        var arr = oVendorDetailModel.getProperty("/address/mainAddress/street");
+                        console.log("success 333" + arr);
 
                     },
                     error: function (oResp) {
@@ -251,29 +251,102 @@ sap.ui.define([
                     }
                 })
             },
-            handelAddCategoryFile: function(oEvent){
-                var oItem = new sap.m.ColumnListItem({
-                    cells: [new sap.m.Input().addStyleClass("voInputText"),new sap.m.Input().addStyleClass("voInputText"),new sap.m.Input().addStyleClass("voInputText"),new sap.m.Input().addStyleClass("voInputText"),new sap.m.Input().addStyleClass("voInputText")]
-                });
-
-                var oTable = this.getView().byId("detailsTable");
-                oTable.addItem(oItem);
-            },
-            onLiveChange: function () {
-                var that = this;
-                var oView = this.getView();
-                var oTable = this.getView().byId("detailsTable");
-                var oModel = oTable.getModel();
-               
-                var payload = {
-                    "firstName" :oView.byId("fName").getValue(),
-                    "name": oView.byId("lName").getValue(),
-                    "telephone": oView.byId("tele").getValue(),
-                    "department": oView.byId("dept").getValue(),
-                    "contactFunction":oView.byId("contactFn").getValue()
+            handelAddCategoryFile: function () {
+                var oVendorDetailModel = this.oVendorDetailModel;
+                var oModel = this.getView().getModel("oVendorDetailModel");
+                var aData = oModel.getProperty("/contacts", []);
+                var newRow = {
+                    "contactId": "5",
+                    "firstName": "",
+                    "lastName": "",
+                    "telephone": "",
+                    "department": " ",
+                    "contactFunction": null,
+                    "vendorNo": "VM000004"
                 }
+
+                aData.push(newRow);
+                oModel.setProperty("/contacts", aData);
+            },
+            onClickAddSectionCompanyCodeInformation: function () {
+                var oVendorDetailModel = this.oVendorDetailModel;
+                var oModel = this.getView().getModel("oVendorDetailModel");
+                var aData = oModel.getProperty("/purchasingOrgInfoDtos", []);
+                var newSection = {
+                    "purchaseId": 5,
+                    "purchaseDetails": null,
+                    "purchaseOrg": "",
+                    "poCurrency": "",
+                    "termsPayment": "",
+                    "incoTerms": "",
+                    "incoTerms2": "",
+                    "priceDataControl": "",
+                    "accVendor": "ACC111",
+                    "shipCondition": "",
+                    "purchaseGrp": "",
+                    "deliveryDate": null,
+                    "confirmationControl": "",
+                    "umg": "sales & distribution",
+                    "roundProfile": "",
+                    "podf": "",
+                    "igrbiv": "",
+                    "abcIndicator": "A",
+                    "salesPerson": "",
+                    "spNo": "",
+                    "oar": "",
+                    "apog": "",
+                    "plant": "",
+                    "subRange": "",
+                    "currency": "",
+                    "ackRequired": "",
+                    "grBasedId": "",
+                    "motBorder": "",
+                    "vendorNo": "VM000004"
+                }
+
+                aData.push(newSection);
+                oModel.setProperty("/purchasingOrgInfoDtos", aData);
+
+            },
+            onClickAddSectionCompanyCodeInformation: function () {
+                var oVendorDetailModel = this.oVendorDetailModel;
+                var oModel = this.getView().getModel("oVendorDetailModel");
+                var aData = oModel.getProperty("/companyCodeInfo", []);
+                var newSection = {
+                    "companyCodeId": 5,
+                    "correspondanceDetails": null,
+                    "companyCode": "3840",
+                    "reconcilliationAccount": "",
+                    "interestInd": "",
+                    "wTaxCode": "",
+                    "whTaxCountry": "",
+                    "recipientType": "",
+                    "exemptionNo": "",
+                    "validUntil": null,
+                    "exemptionAuthority": "",
+                    "paymentTerms": "",
+                    "toleranceGroup": "",
+                    "paymentMethods": "",
+                    "paymentBlock": "",
+                    "houseBank": "",
+                    "groupingKey": "",
+                    "accountingClerk": "Max",
+                    "accountingClerksFax": "FAX001",
+                    "accountingClerksTelephone": "098765",
+                    "clerkAtSupplier": "",
+                    "planningGroup": "",
+                    "minorityIndicator": "",
+                    "clerkEmail": "max@gmail.com",
+                    "reconcilliationAccountInGeneralLedger": "",
+                    "checkFlagForDoubleInvoiceOrCreditMemo": "",
+                    "companyCodeDeletionBlock": "",
+                    "sortKeyForPaymentAssignment": "",
+                    "vendorNo": "VM000004"
+                }
+                aData.push(newSection);
+                oModel.setProperty("/companyCodeInfo", aData);
+
             }
 
-            
         });
     });
